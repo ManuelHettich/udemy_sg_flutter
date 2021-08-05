972e719acc991b23330e808b43a40e7a2034fc30
Manuel Hettich <17690367+ManuelHettich@users.noreply.github.com>
2021-08-05 13:20:40 +0200

    Start project login_stateful

---
+++ login_stateful/lib/src/screens/login_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return LoginScreenState();
+  }
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  Widget build(context) {
+    return Container(
+      margin: EdgeInsets.all(20.0),
+      child: Form(
+        child: Column(
+          children: [
+            emailField(),
+            // passwordField(),
+            // submitButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget emailField() {
+    return TextFormField(
+      decoration: InputDecoration(
+        labelText: 'Email Address',
+        hintText: 'email@example.com',
+      ),
+    );
+  }
+
+  Widget passwordField() {
+    throw Exception();
+  }
+
+  Widget submitButton() {
+    throw Exception();
+  }
+}
